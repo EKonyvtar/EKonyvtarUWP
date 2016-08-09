bbc5fee0c78767709d35a5b646b5bb86422b0617
Akos Murati <akos.murati@xero.com>
2016-08-09 15:49:14 +1200

    Prettiify BookControl

--- EKonyvtarUW/EKonyvtarUW/Views/BookControl.xaml.cs
+++ EKonyvtarUW/EKonyvtarUW/Views/BookControl.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -20,9 +7,13 @@ namespace EKonyvtarUW.Views
     public sealed partial class BookControl : UserControl
     {
 
+        //private Compositor _compositor;
+
         public BookControl()
         {
             this.InitializeComponent();
+            //_compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+            //Loaded += BookControl_Loaded; ;
         }
     }
 }
